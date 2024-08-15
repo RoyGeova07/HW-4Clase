de1aab88852223ddf8f2fdc4b82549ee0ecd21ab
RoyGeova07 <royumana3@gmail.com>
2024-08-15 15:24:16 -0600

    Holaaaaaa

---
+++ OneDrive/Documentos/NetBeansProjects/Proyecto_Twit_Roy_Moises/src/proyecto_twit_roy_moises/Proyecto_Twit_Roy_Moises.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto_twit_roy_moises;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author royum
+ */
+public class Proyecto_Twit_Roy_Moises {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner lea = new Scanner (System.in);
+        
+        
+        
+        
+    }
+    
+}
